dd9236b3476b30dadd0ba12afedb1ad2495f29f8
Matthew Hamilton <matthamilton@live.com>
2019-11-11 01:55:55 -0600

    Use switch expressions.

--- src/FluentFiles.Tests/FixedLength/FixedLengthMasterDetailCustomTests.cs
+++ src/FluentFiles.Tests/FixedLength/FixedLengthMasterDetailCustomTests.cs
@@ -149,19 +149,14 @@ D20150512Standalone                     ";
                                                     {
                                                         if (String.IsNullOrEmpty(s) || s.Length < 1) return null;
 
-                                                        switch (s[0])
+                                                        return (s[0]) switch
                                                         {
-                                                            case 'H':
-                                                                return typeof(HeaderRecord);
-                                                            case 'M':
-                                                                return typeof(HeaderRecordContinuation);
-                                                            case 'S':
-                                                                return typeof(StandaloneRecord);
-                                                            case 'D':
-                                                                return typeof(DetailRecord);
-                                                            default:
-                                                                return null;
-                                                        }
+                                                            'H' => typeof(HeaderRecord),
+                                                            'M' => typeof(HeaderRecordContinuation),
+                                                            'S' => typeof(StandaloneRecord),
+                                                            'D' => typeof(DetailRecord),
+                                                            _ => null,
+                                                        };
                                                     },
                                                     new FixedLengthLineBuilderFactory(),
                                                     new FixedLengthLineParserFactory(),
